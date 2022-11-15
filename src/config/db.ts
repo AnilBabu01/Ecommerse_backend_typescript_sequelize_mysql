13f045423e28c9f8008b2213ca4de0e558becf08
anil babu <anilb9850@gmail.com>
2022-11-15 19:51:35 +0530

    assingment done

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,31 +1,8 @@
-import { Sequelize} from 'sequelize-typescript'
-import {User} from '../models/user'
+import { Sequelize } from "sequelize-typescript";
+import { User } from "../models/user";
 
-export  const sequelize = new Sequelize("test", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-    models: [User]
+export const sequelize = new Sequelize("test", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  models: [User],
 });
-
-
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch((error:any) => {
-//     console.error("Unable to connect to the database: ", error);
-//   });
-
-
-  
-//   sequelize
-//     .sync({ force: false })
-//     .then(() => {
-//       console.log("Re-sync successfully!");
-//     })
-//     .catch((error) => {
-//       console.error("Unable to Re-sync : ", error);
-//     });
-  
