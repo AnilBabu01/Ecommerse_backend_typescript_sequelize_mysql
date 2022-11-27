b6bcbd59c92ab365d08ea16966e030924e8a05ce
anil babu <anilb9850@gmail.com>
2022-11-27 21:13:34 +0530

    Woring on order  apis

--- src/controllers/orderControllers.ts
+++ src/controllers/orderControllers.ts
@@ -17,19 +17,65 @@ export const newOrder: RequestHandler = async (req, res) => {
         paymentInfo,
       } = req.body;
 
-      console.log(
-        orderItems,
-        shippingInfo,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paymentInfo
-      );
+      let order: any;
+      let orderitems: any;
+      let payinfo: any;
+      console.log(req.user.userid);
 
-      res.status(200).json({
-        success: true,
-      });
+      //   order = await Order.create({
+      //     address: shippingInfo.address,
+      //     phone: shippingInfo.phone,
+      //     postalCode: shippingInfo.postalCode,
+      //     country: shippingInfo.country,
+      //     paidAt: Date.now(),
+      //     itemsPrice: Number(itemsPrice),
+      //     taxPrice: taxPrice,
+      //     shippingPrice: shippingInfo,
+      //     totalPrice: Number(totalPrice),
+      //     deliveredAt: Date.now(),
+      //     userid: req.user.userid,
+      //   });
+
+      //   if (order) {
+      //     if (orderItems) {
+      //       for (let i = 0; i < orderItems.length; i++) {
+      //         await Orderitems.create({
+      //           name: orderItems[i].name,
+      //           quantity: orderItems[i].quantity,
+      //           image: orderItems[i].name.image,
+      //           price: orderItems[i].name.price,
+      //           orderid: order.orderid,
+      //         });
+      //       }
+      //     }
+      //     if (paymentInfo) {
+      //       payinfo = await Paymentinfo.create({
+      //         paymentid: paymentInfo.id,
+      //         status: paymentInfo.status,
+      //         orderid: order.orderid,
+      //       });
+      //     }
+
+      //     orderitems = await Orderitems.findAll({
+      //       where: { orderid: order.orderid },
+      //     });
+      //   }
+      //   console.log(
+      //     orderItems,
+      //     shippingInfo,
+
+      //     taxPrice,
+      //     shippingPrice,
+      //     totalPrice,
+      //     paymentInfo
+      //   );
+
+      //   res.status(200).json({
+      //     success: true,
+      //     order: order,
+      //     orderitems: orderitems,
+      //     payinfo: payinfo,
+      //   });
     }
   } catch (error) {}
 };
